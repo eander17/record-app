9924c474c51888ead630f0b7c537163a08ce9234
eander17 <89489903+eander17@users.noreply.github.com>
2023-04-20 15:00:54 -0600

    Delete frontend directory

--- frontend/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-
-// IS valid with React 18
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
