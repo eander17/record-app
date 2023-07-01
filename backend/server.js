2c83e0d802a5dbb928ce6bc292d261763137c6cd
evanio <eander17@uccs.edu>
2023-07-01 09:15:06 -0600

    testing lint-staged

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,8 @@ const server = require('http').createServer(app)
 
 const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000'
 
+// td: separate cors from socket.io
+// eslint-disable-next-line no-unused-vars
 const io = require('socket.io')(server, {
   cors: {
     origin: CLIENT_ORIGIN,
@@ -37,64 +39,65 @@ const connectDB = require('./config/db')
 dotenv.config() // loads the environment variables
 connectDB() // connects to the database
 
-const albumUserMap = new Map() // map of album ids to users
-
 /// Routing Section ///
 app.use('/api/collection', require('./routes/albumCollectionRoutes')) // routes for albums
 app.use('/api/users', require('./routes/userRoutes')) // routes for users
 app.use('/api/search', require('./routes/discogRoutes')) // routes for discogs search
 
 // register the error handler middleware.
-//! !! This must be registered last !!!
+//! !! This middleware must be registered last !!!
 app.use(errorHandler)
 
+// td determine if I want to use socket.io - #project
+// td refactor socket io to separate file - #server.js
 /// Socket IO Section ///
-// add a socket.io connection listener
-io.on('connection', (socket) => {
-  console.log('client connected: ')
+// // add a socket.io connection listener
+// const albumUserMap = new Map() // map of album ids to users
+// io.on('connection', (socket) => {
+//   console.log('client connected: ')
 
-  //* joinAlbumRoom - Listens for a user to join a room
-  // ? room === discogsId
-  // ? id === userId
-  // ? join room listener
-  socket.on('joinAlbumRoom', (data) => {
-    const { room, id } = data
-    socket.join(room)
-    socket.join(id)
+//   // joinAlbumRoom - Listens for a user to join a room
+//   // ? room === discogsId
+//   // ? id === userId
+//   // ? join room listener
+//   socket.on('joinAlbumRoom', (data) => {
+//     const { room, id } = data
+//     socket.join(room)
+//     socket.join(id)
 
-    if (!albumUserMap.has(room)) {
-      albumUserMap.set(room, new Set([id]))
-    } else {
-      albumUserMap.get(room).add(id)
-    }
-  })
+//     if (!albumUserMap.has(room)) {
+//       albumUserMap.set(room, new Set([id]))
+//     } else {
+//       albumUserMap.get(room).add(id)
+//     }
+//   })
 
-  //* listen for events from client
-  socket.on('emitCustomFieldUpdate', (data) => {
-    const { room, id, key, value } = data
+//   // listen for events from client
+//   socket.on('emitCustomFieldUpdate', (data) => {
+//     const { room, id, key, value } = data
 
-    const albumUsers = albumUserMap.get(room) || new Set() // get the set of users in the room
+//     const albumUsers = albumUserMap.get(room) || new Set() // get the set of users in the room
 
-    // ? if the user is the only one in the room, don't send the update
-    if (albumUsers.size === 1 && albumUsers.has(id)) return
+//     // ? if the user is the only one in the room, don't send the update
+//     if (albumUsers.size === 1 && albumUsers.has(id)) return
 
-    // ? emit the updated custom field data to all connected clients in the room
-    albumUsers.forEach((userId) => {
-      if (userId !== id) {
-        io.to(userId).emit('notifyCustomFieldUpdate', {
-          discogId: room,
-          key,
-          value,
-        })
-      }
-    })
-  })
+//     // ? emit the updated custom field data to all connected clients in the room
+//     albumUsers.forEach((userId) => {
+//       if (userId !== id) {
+//         io.to(userId).emit('notifyCustomFieldUpdate', {
+//           discogId: room,
+//           key,
+//           value,
+//         })
+//       }
+//     })
+//   })
 
-  // disconnect listener
-  socket.on('disconnect', () => {
-    console.log('user disconnected')
-  })
-})
+//   // disconnect listener
+//   socket.on('disconnect', () => {
+//     console.log('user disconnected')
+//   })
+// })
 
 /// Serve Application ///
 // serve front end
